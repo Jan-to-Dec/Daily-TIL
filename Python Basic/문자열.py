2aa8005d75d734948f5c27810ee7461630a272fb
JongHun <113554766+springtowinter-data@users.noreply.github.com>
2024-07-08 16:45:31 +0900

    new Repository

---
+++ Python Basic/문자열.py
@@ -0,0 +1,15 @@
+# 리스트명 = [요소1, 요소2, 요소3, ...]
+
+a = [1, 2, 3]
+print(a)
+
+str1 = "Hello"
+str2 = "World"
+result = str1 + ", " + str2 + "!"
+print(result)  
+
+str = "Life is too short, You need Python."
+print(str[12])
+print(str[0:12])
+print(str[:12])
+print(str[12:])
---
+++ Python Basic/숫자형.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+# 정수형 
+a = 10
+b = 5
+
+# 기본적인 산술 연산
+print(a + b)  # 더하기
+print(a - b)  # 빼기
+print(a * b)  # 곱하기
+print(a // b) # 나누기(정수 나눗셈)
+print(a % b)  # 나머지
+print(a ** b) # 거듭제곱
+
+
+# float
+x = 10.5
+y = 4.2
+
+print(x + y)  # 더하기
+print(x - y)  # 빼기
+print(x * y)  # 곱하기
+print(x / y)  # 나누기
+print(x ** y) # 거듭제곱
+
