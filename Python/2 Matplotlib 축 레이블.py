63fb29a2ace27acacc8dc823c7a4651acce93c23
JongHun <113554766+springtowinter-data@users.noreply.github.com>
2024-07-12 23:14:42 +0900

    0712 summary

---
+++ Python/2 Matplotlib 축 레이블.py
@@ -0,0 +1,43 @@
+#%%
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.xlabel('X-Label')
+plt.ylabel('Y-Label')
+plt.show()
+
+#%%
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+plt.xlabel('X-Axis', labelpad=15)
+plt.ylabel('Y-Axis', labelpad=20)
+plt.show()
+
+# %%
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+plt.xlabel('X-Axis', labelpad=15, fontdict={'family': 'serif', 'color': 'b', 'weight': 'bold', 'size': 14})
+plt.ylabel('Y-Axis', labelpad=20, fontdict={'family': 'fantasy', 'color': 'green', 'weight': 'normal', 'size': 'xx-large'})
+plt.show()
+# %%
+import matplotlib.pyplot as plt
+
+font1 = {'family': 'serif',
+         'color': 'b',
+         'weight': 'bold',
+         'size': 14
+         }
+
+font2 = {'family': 'fantasy',
+         'color': 'blue',
+         'weight': 'normal',
+         'size': 'xx-large'
+         }
+
+plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+plt.xlabel('X-Axis', labelpad=15, fontdict=font1)
+plt.ylabel('Y-Axis', labelpad=20, fontdict=font2)
+plt.show()
+# %%
