a0ea61ae533ffb28ad57f2d82c14764fbb24ede1
JongHun <113554766+springtowinter-data@users.noreply.github.com>
2024-07-13 17:02:08 +0900

    add - 0713

--- Python/4 Matplotlib 축 범위.py
+++ Python/4 Matplotlib 축 범위.py
@@ -8,18 +8,16 @@ plt.xlim([0, 5])      # X축의 범위: [xmin, xmax]
 plt.ylim([0, 20])     # Y축의 범위: [ymin, ymax]
 
 plt.show()
-# %%
+#%%
 import matplotlib.pyplot as plt
 
 plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
 plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
 plt.axis([0, 5, 0, 20])  # X, Y축의 범위: [xmin, xmax, ymin, ymax]
-
 plt.show()
-# %%
-import matplotlib.pyplot as plt
 
+#%%
 plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
 plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
