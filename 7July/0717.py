8405b28869d2f8e3080ba2052ac1f27395770222
JongHun <113554766+springtowinter-data@users.noreply.github.com>
2024-07-17 15:07:50 +0900

    updated 0717

---
+++ 7July/0717.py
@@ -0,0 +1,35 @@
+# 메타코드 
+
+# Data Type 
+
+# 숫자형의 종류
+# int()
+# float()
+
+a = int(3)
+b = 4
+c = float(2)
+d = 2.0
+
+
+# 정수와 실수의 뎃셈 뺄셈의 결과는 실수
+e = a + c
+f = a - c
+g = a * c
+h = c /d
+
+# boolean 
+3>4
+3<4
+3==4
+3!=4
+
+# 복합 대입 연산자 
+a = 3
+a +=2
+print(a)
+
+# 문자열 string 
+a = 'thank'
+b = 'you'
+
